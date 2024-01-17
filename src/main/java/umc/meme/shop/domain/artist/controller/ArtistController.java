5b50dd9cee96e6dcbfd6b36ffc55070fbd45451c
daeun084 <030804jk@naver.com>
2024-01-17 23:44:15 +0900

    add: exception handling

--- src/main/java/umc/meme/shop/domain/artist/controller/ArtistController.java
+++ src/main/java/umc/meme/shop/domain/artist/controller/ArtistController.java
@@ -1,11 +1,33 @@
 package umc.meme.shop.domain.artist.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import umc.meme.shop.domain.artist.service.ArtistService;
+import umc.meme.shop.global.ErrorStatus;
+import umc.meme.shop.global.SuccessStatus;
+import umc.meme.shop.global.exception.GlobalException;
+import umc.meme.shop.global.exception.GlobalExceptionHandler;
+import umc.meme.shop.global.response.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
 public class ArtistController {
     private final ArtistService artistService;
+
+    //ApiResponse TEST
+    @GetMapping("/temp/success")
+    public ApiResponse tempSuccess(){
+        return ApiResponse.SuccessResponse(SuccessStatus.TEMP);
+    }
+    @GetMapping("/temp/failure")
+    public ApiResponse tempFailure(){
+        return ApiResponse.FailureResponse(ErrorStatus.TEMP);
+    }
+
+    @GetMapping("/temp/exception")
+    public ApiResponse tempException(){
+        throw new GlobalException(ErrorStatus.TEMP);
+//        return ApiResponse.SuccessResponse(SuccessStatus.TEMP);
+    }
 }
--- src/main/java/umc/meme/shop/global/response/ApiResponse.java
+++ src/main/java/umc/meme/shop/global/response/ApiResponse.java
@@ -1,5 +1,7 @@
 package umc.meme.shop.global.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import umc.meme.shop.global.ErrorStatus;
@@ -7,10 +9,13 @@ import umc.meme.shop.global.SuccessStatus;
 
 @Getter
 @AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "statusCode", "message", "data"})
 public class ApiResponse<T> {
+
     private final String isSuccess;
     private final int statusCode;
     private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final T data;
 
     public static <T>ApiResponse<T> SuccessResponse(SuccessStatus status, T data){
