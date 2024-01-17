4921c211ad9c39f285fd39b90279be705eeb5d0d
daeun084 <030804jk@naver.com>
2024-01-18 00:09:16 +0900

    add: mypage controller mapping

---
+++ src/main/java/umc/meme/shop/domain/mypage/controller/MyPageController.java
@@ -0,0 +1,36 @@
+package umc.meme.shop.domain.mypage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import umc.meme.shop.global.SuccessStatus;
+import umc.meme.shop.global.response.ApiResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/mypage")
+public class MyPageController {
+
+//    @Operation(summary = "마이페이지")
+    @GetMapping("/{userId}")
+    public ApiResponse mypage(){
+        return ApiResponse.SuccessResponse(SuccessStatus.TEMP, "");
+    }
+
+//    @Operation(summary = "내 정보 수정")
+    @PatchMapping("/{userId}/details")
+    public ApiResponse details(){
+        return ApiResponse.SuccessResponse(SuccessStatus.TEMP, "");
+    }
+
+//    @Operation(summary = "약관 및 정책")
+    @GetMapping("/tos")
+    public ApiResponse tos(){
+        return ApiResponse.SuccessResponse(SuccessStatus.TEMP, "");
+    }
+
+//    @Operation(summary = "문의하기")
+    @PostMapping("/contact")
+    public ApiResponse contact(){
+        return ApiResponse.SuccessResponse(SuccessStatus.TEMP, "");
+    }
+}
