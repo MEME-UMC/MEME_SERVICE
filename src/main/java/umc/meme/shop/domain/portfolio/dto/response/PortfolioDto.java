61b7f92398e9c603d899275716e770e21a39e70f
daeun084 <030804jk@naver.com>
2024-01-25 19:18:19 +0900

    chore: portfolio 조회에 reviewList 추가

--- src/main/java/umc/meme/shop/domain/portfolio/dto/response/PortfolioDto.java
+++ src/main/java/umc/meme/shop/domain/portfolio/dto/response/PortfolioDto.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 import umc.meme.shop.domain.favorite.entity.FavoritePortfolio;
 import umc.meme.shop.domain.portfolio.entity.Portfolio;
 import umc.meme.shop.domain.portfolio.entity.enums.Category;
+import umc.meme.shop.domain.review.dto.response.ReviewResponseDto;
+
+import java.util.List;
 
 @Getter
 @Builder
@@ -26,7 +29,13 @@ public class PortfolioDto {
 
     private Boolean isBlock;
 
+    private List<ReviewResponseDto> reviewResponseDtoList;
+
     public static PortfolioDto from(Portfolio portfolio){
+        List<ReviewResponseDto> reviewResponseDtoList = portfolio.getReviewList()
+                .stream()
+                .map(ReviewResponseDto::from)
+                .toList();
         return PortfolioDto.builder()
                 .portfolioId(portfolio.getPortfolioId())
                 .category(portfolio.getCategory())
@@ -34,12 +43,19 @@ public class PortfolioDto {
                 .price(portfolio.getPrice())
                 .info(portfolio.getInfo())
                 .isBlock(portfolio.isBlock())
+                .reviewResponseDtoList(reviewResponseDtoList)
                 .build();
     }
 
     //관심 메이크업
     public static PortfolioDto from(FavoritePortfolio favoritePortfolio){
         Portfolio portfolio = favoritePortfolio.getPortfolio();
+
+        List<ReviewResponseDto> reviewResponseDtoList = portfolio.getReviewList()
+                .stream()
+                .map(ReviewResponseDto::from)
+                .toList();
+
         return PortfolioDto.builder()
                 .portfolioId(portfolio.getPortfolioId())
                 .category(portfolio.getCategory())
@@ -47,6 +63,7 @@ public class PortfolioDto {
                 .price(portfolio.getPrice())
                 .info(portfolio.getInfo())
                 .isBlock(portfolio.isBlock())
+                .reviewResponseDtoList(reviewResponseDtoList)
                 .build();
     }
 }
