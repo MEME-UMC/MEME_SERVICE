2f2aa8ac24bf65d1b6669283d776cb5cfa922ddd
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-01-31 22:00:46 +0900

    feat: test deleted

--- src/test/java/umc/meme/shop/ShopApplicationTests.java
+++ src/test/java/umc/meme/shop/ShopApplicationTests.java
@@ -1,13 +1,13 @@
-package umc.meme.shop;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package umc.meme.shop;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ShopApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
