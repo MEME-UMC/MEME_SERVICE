65c6a528d3851ccd5b6f401edd467e2c86ec9697
daeun084 <030804jk@naver.com>
2024-02-02 19:53:34 +0900

    chore: 포트폴리오 dto 수정

--- src/main/java/umc/meme/shop/domain/portfolio/dto/response/PortfolioDto.java
+++ src/main/java/umc/meme/shop/domain/portfolio/dto/response/PortfolioDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import umc.meme.shop.domain.artist.entity.Artist;
+import umc.meme.shop.domain.artist.entity.enums.MakeupLocation;
 import umc.meme.shop.domain.favorite.entity.FavoritePortfolio;
 import umc.meme.shop.domain.portfolio.entity.Portfolio;
 import umc.meme.shop.domain.portfolio.entity.enums.Category;
@@ -20,11 +22,19 @@ public class PortfolioDto {
 
     private Category category;
 
+    private String artistName;
+
     private String makeupName;
 
     private int price;
 
     private String info;
+
+    private MakeupLocation makeupLocation; //샵 재직 여부
+
+    private String shopLocation; //샵 위치
+
+    private List<String> region; //활동 가능 지역
 
     private Boolean isBlock;
 
@@ -36,6 +46,7 @@ public class PortfolioDto {
 
 
     public static PortfolioDto from(Portfolio portfolio) {
+        Artist artist = portfolio.getArtist();
 
         // PortfolioImg 리스트를 PortfolioImgDto 리스트로 변환
         List<PortfolioImgDto> portfolioImgDtoList = portfolio.getPortfolioImgList()
@@ -46,9 +57,13 @@ public class PortfolioDto {
         return PortfolioDto.builder()
                 .portfolioId(portfolio.getPortfolioId())
                 .category(portfolio.getCategory())
+                .artistName(artist.getName())
                 .makeupName(portfolio.getMakeupName())
                 .price(portfolio.getPrice())
                 .info(portfolio.getInfo())
+                .makeupLocation(artist.getMakeupLocation())
+                .shopLocation(artist.getShopLocation())
+                .region(artist.getRegion())
                 .isBlock(portfolio.isBlock())
                 .portfolioImgDtoList(portfolioImgDtoList)
                 .averageStars(portfolio.getAverageStars())
