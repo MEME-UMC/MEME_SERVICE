9c041183c79073df302f3226ae35d4a9c501c2fd
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-02-02 23:34:26 +0900

    fix: ReviewResponseDto

--- src/main/java/umc/meme/shop/ShopApplication.java
+++ src/main/java/umc/meme/shop/ShopApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class ShopApplication {
 
-	public static void main(String[] args) { SpringApplication.run(ShopApplication.class, args); }
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
 
 }
