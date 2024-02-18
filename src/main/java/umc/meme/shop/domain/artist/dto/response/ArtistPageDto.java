1c54dcaa64fe9a5ad6b6bd76ec52fcc3ad92d093
daeun084 <030804jk@naver.com>
2024-02-18 14:14:22 +0900

    fix: dev conflict

--- src/main/java/umc/meme/shop/domain/artist/dto/response/ArtistPageDto.java
+++ src/main/java/umc/meme/shop/domain/artist/dto/response/ArtistPageDto.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import umc.meme.shop.domain.portfolio.dto.response.PortfolioDto;
 
 import java.util.List;
 
