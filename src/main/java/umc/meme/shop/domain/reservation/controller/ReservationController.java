1ed6ca545b570671bb446d8acae625e65e8ec59a
daeun084 <030804jk@naver.com>
2024-03-25 14:55:49 +0900

    fix: devc reservation fix

--- src/main/java/umc/meme/shop/domain/reservation/controller/ReservationController.java
+++ src/main/java/umc/meme/shop/domain/reservation/controller/ReservationController.java
@@ -54,10 +54,24 @@ public class ReservationController {
         return ApiResponse.SuccessResponse(SuccessStatus.RESERVATION_GET, reservationService.getArtistReservation(artistId));
     }
 
+    //아티스트 예약 상세 조회
+    @Operation(summary = "아티스트 예약 상세 조회", description = "Artist ver. 예약 정보를 상세조회하는 API입니다.")
+    @GetMapping("/{reservationId}/artist/details")
+    public ApiResponse getArtistReservationDetails(@PathVariable(name = "reservationId") Long reservationId){
+        return ApiResponse.SuccessResponse(SuccessStatus.RESERVATION_DETAILS_GET, reservationService.getArtistDetailsReservation(reservationId));
+    }
+
     //모델 예약 조회
     @Operation(summary = "모델 예약 조회", description = "예약 정보를 조회하는 API입니다.")
     @GetMapping("/{modelId}/model")
     public ApiResponse getModelReservation(@PathVariable Long modelId){
         return ApiResponse.SuccessResponse(SuccessStatus.RESERVATION_GET, reservationService.getModelReservation(modelId));
+    }
+
+    //모델 예약 상세 조회
+    @Operation(summary = "모델 예약 상세 조회", description = "Model ver. 예약 정보를 상세조회하는 API입니다.")
+    @GetMapping("/{reservationId}/model/details")
+    public ApiResponse getModelReservationDetails(@PathVariable(name = "reservationId") Long reservationId){
+        return ApiResponse.SuccessResponse(SuccessStatus.RESERVATION_DETAILS_GET, reservationService.getModelDetailsReservation(reservationId));
     }
 }
