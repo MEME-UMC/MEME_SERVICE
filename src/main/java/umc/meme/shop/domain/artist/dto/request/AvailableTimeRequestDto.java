1845457e11d8175729eb9dc6cfceb2d5abb6cc61
daeun084 <030804jk@naver.com>
2024-03-30 19:10:07 +0900

    chore: available time 관련 dto 필드 수정

--- src/main/java/umc/meme/shop/domain/artist/dto/request/AvailableTimeRequestDto.java
+++ src/main/java/umc/meme/shop/domain/artist/dto/request/AvailableTimeRequestDto.java
@@ -4,26 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import umc.meme.shop.domain.artist.entity.AvailableTime;
 import umc.meme.shop.global.enums.DayOfWeek;
 import umc.meme.shop.global.enums.Times;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AvailableTimeRequestDto {
-    private Date date; //날짜
-    private DayOfWeek dayOfWeek; //요일
-    private Times times;
+    private Long userId;
+    private List<AvailableTimeDto> availableTimeDtoList;
 
-    static public AvailableTimeRequestDto from(AvailableTime availableTime){
-        return AvailableTimeRequestDto.builder()
-                .date(availableTime.getDate())
-                .dayOfWeek(availableTime.getDayOfWeek())
-                .times(availableTime.getTimes())
-                .build();
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+        public static class AvailableTimeDto {
+            private Date date; //날짜
+            private DayOfWeek dayOfWeek; //요일
+            private Times times;
+
     }
 }
