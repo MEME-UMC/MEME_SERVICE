3dca79f3c7bae929cd7d64a2fcdfd3b89c8b14f8
daeun084 <030804jk@naver.com>
2024-04-21 10:51:15 +0900

    refactor: 예약 상세 조회 API field 수정, artist ver, model ver 조회 API 통합

--- src/main/java/umc/meme/shop/domain/reservation/dto/response/ReservationDetailDto.java
+++ src/main/java/umc/meme/shop/domain/reservation/dto/response/ReservationDetailDto.java
@@ -5,15 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import umc.meme.shop.domain.artist.entity.Artist;
+import umc.meme.shop.domain.model.entity.Model;
 import umc.meme.shop.domain.portfolio.entity.Portfolio;
 import umc.meme.shop.domain.reservation.entity.Reservation;
-import umc.meme.shop.global.enums.Category;
+import umc.meme.shop.global.enums.*;
+
+import java.util.Date;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ModelReservationDetailDto {
+public class ReservationDetailDto {
     private Long reservationId;
     private Long portfolioId;
     //아티스트 정보
@@ -21,24 +24,33 @@ public class ModelReservationDetailDto {
     private String artistProfileImg;
     //예약 정보
     private String portfolioName;
-    private String portfolioImg;
     private Category category;
+    private Date reservationDate;
+    private Times reservationTime;
     private String location;
     private int price;
+    //모델 정보
+    private Gender gender;
+    private SkinType skinType;
+    private PersonalColor personalColor;
 
-    public static ModelReservationDetailDto from(Reservation reservation){
+    public static ReservationDetailDto from(Reservation reservation, Model model){
         Portfolio portfolio = reservation.getPortfolio();
         Artist artist = portfolio.getArtist();
-        return ModelReservationDetailDto.builder()
+        return ReservationDetailDto.builder()
                 .reservationId(reservation.getReservationId())
                 .portfolioId(portfolio.getPortfolioId())
                 .artistNickName(artist.getNickname())
                 .artistProfileImg(artist.getProfileImg())
                 .portfolioName(portfolio.getMakeupName())
-                .portfolioImg(portfolio.getPortfolioImgList().get(0).getSrc())
                 .category(portfolio.getCategory())
+                .reservationDate(reservation.getAvailableTime().getDate())
+                .reservationTime(reservation.getAvailableTime().getTimes())
                 .location(reservation.getLocation())
                 .price(portfolio.getPrice())
+                .gender(reservation.getModel().getGender())
+                .skinType(model.getSkinType())
+                .personalColor(model.getPersonalColor())
                 .build();
     }
 
